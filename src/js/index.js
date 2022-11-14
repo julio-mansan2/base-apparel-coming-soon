d4bd980cc2e4881491bc6300cdbdf9cb7bd43c86
Júlio Mansan <julio.mansan31@gmail.com>
2022-11-13 23:01:24 -0300

    Validação do email

--- src/js/index.js
+++ src/js/index.js
@@ -4,24 +4,19 @@ const errorMessage = document.querySelector('.error-message')
 const errorIcon = document.querySelector('.icon-error')
 const form = document.querySelector('form')
 
-function validaEmail(email) {
-    const emailRegex =  /^([a-zA-Z][^<>\"!@[\]#$%¨&*()~^:;ç,\-´`=+{}º\|/\\?]{1,})@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return emailRegex.test(String(email).toLowerCase())
-}
 
-inputSubmit.addEventListener ('click', (email) =>   
+inputSubmit.addEventListener ('click', () =>   
 {
     event.preventDefault()
-    validaEmail()
-    if (validaEmail === true) {            
-        inputText.classList.remove("borda-vermelha")
-        errorMessage.classList.remove("aparecer")
-        errorIcon.classList.remove("aparecer")
-    } else {
+    if ((inputText.value === "" || inputText.validity.valid === false)) {            
         inputText.classList.remove("borda-normal")
         inputText.classList.add("borda-vermelha")
         errorMessage.classList.add("aparecer")
         errorIcon.classList.add("aparecer")
+    } else {
+        inputText.classList.remove("borda-vermelha")
+        errorMessage.classList.remove("aparecer")
+        errorIcon.classList.remove("aparecer")
     }
 })
 
