6be254439d779626c4791ec2baaa89c3d79714af
Júlio Mansan <julio.mansan31@gmail.com>
2022-11-15 16:40:38 -0300

    Alteração no icon-error e na borda do input

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 const inputSubmit = document.getElementById('submit')
 const inputText = document.getElementById('email')
 const errorMessage = document.querySelector('.error-message')
-const errorIcon = document.querySelector('.icon-error')
 const form = document.querySelector('form')
 
 
@@ -11,12 +10,13 @@ inputSubmit.addEventListener ('click', () =>
     if ((inputText.value === "" || inputText.validity.valid === false)) {            
         inputText.classList.remove("borda-normal")
         inputText.classList.add("borda-vermelha")
+        inputText.classList.add('icon-error')
         errorMessage.classList.add("aparecer")
-        errorIcon.classList.add("aparecer")
     } else {
         inputText.classList.remove("borda-vermelha")
+        inputText.classList.add("borda-normal")
         errorMessage.classList.remove("aparecer")
-        errorIcon.classList.remove("aparecer")
+        inputText.classList.remove("icon-error")
     }
 })
 
